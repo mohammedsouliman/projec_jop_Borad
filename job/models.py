a52b935a4d684d0feba37e20048d7a3d39f2085a
Muhammed Souliman <mohammed.python.java@gmail.com>
2020-12-27 07:33:02 -0800

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+
+django model field :
+  -html widget
+  -validation
+  -db size
+
+'''
+JOB_TYPE =(
+    ('Full Time','Full Time'),
+    ('Part Time', 'Part Time'),
+)
+
+
+class  job (models.Model):
+    title = models.CharField(max_length=100) # column
+    #location
+    job_type = models.CharField( max_length=20  , choices = JOB_TYPE  )
+    description = models.TextField(max_length =1000)
+    punlished_at = models.DateTimeField( auto_now=True , auto_now_add=False)
+    vacancy = models.IntegerField(default = 1)
+    salary = models.FloatField(default =0) 
+    experience = models.IntegerField(default = 1)
+
+
+    def  __str__(self):
+        return self.title
+    
---
+++ outlne.md
@@ -0,0 +1,31 @@
+job:
+    -title
+   # -location
+    -job type
+    -description
+    -punlished at 
+    -vacancy 
+    -salary
+   # -category
+    -experience
+
+
+    -apply job 
+    -post job
+
+
+
+مهنة:
+    -عنوان
+    -موقعك
+    -نوع الوظيفة
+    -وصف
+    - تم تحريره في
+    -شاغر
+    -راتب
+    -الفئة
+    -تجربة
+
+
+    -التقدم لوظيفة
+    -وظيفة بعد
